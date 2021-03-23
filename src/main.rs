358f077882f22d54331afaeacaf61e7a9e4a41e5
Kenneth Price <51425692+SneakyLift@users.noreply.github.com>
2021-03-22 18:43:53 -0700

    uses mutex now, more threads

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,41 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Write, Read};
 use std::thread;
 use std::time::{Duration, SystemTime};
-use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
 
 
 fn main() {
-
-    let listener = match TcpListener::bind("127.0.0.1:8080") {
-        Ok(t) => t,
-        Err(e) => panic!("Listener failed to initialize. Error: {}", e),
-    };
+    let server_on = Arc::new(Mutex::new(true));
+    let server_update = Arc::new(Mutex::new(false));
     let destination:String = String::from("127.0.0.1:8080");
-    let (sender1, receiver1) = mpsc::sync_channel(0);
-    let (sender2, receiver2) = mpsc::sync_channel(0);
+    let source:String = String::from("127.0.0.1:8081");
+    let source_listener = TcpListener::bind(&source).unwrap();
+    let dest_listener = TcpListener::bind(&destination).unwrap();
+
+    //why the fuck is this necessary
+    //this cant possibly be necessary
+    let server_on1 = server_on.clone();
+    let server_on2 = server_on.clone();
+    let server_on3 = server_on.clone();
+    let server_update1 = server_update.clone();
+    let server_update2 = server_update.clone();
+    let server_update3 = server_update.clone();
     //process connections continuously in its own thread
-    thread::spawn(||listener_thread(listener, destination,
-                                    receiver2, sender1));
+    thread::spawn(move ||{
+        listener_thread(source_listener, destination,
+                        server_on1.clone(),
+                        server_update1.clone())
+    });
+    thread::spawn(move||listener_thread(dest_listener, source, server_on2.clone(),
+    server_update2.clone()));
     //Separate thread checking how long the server has been inactive
-    thread::spawn(|| {server_switch_thread(receiver1, sender2)});
+    thread::spawn(move || {server_switch_thread(server_on3.clone(),
+                                           server_update3.clone())});
     loop{}
 }
 //To be called by a listener thread. Listens on a port and handles connections
-fn listener_thread(listener: TcpListener, dest: String, receiver: mpsc::Receiver<bool>,
-                   sender: mpsc::SyncSender<bool>) {
+fn listener_thread(listener: TcpListener, dest: String, server_on: Arc<Mutex<bool>>,
+                    server_update: Arc<Mutex<bool>>) {
     //this loop checks that the streams are valid, sets the server_on bool, and handles connections.
     //listener.incoming() causes this to loop indefinitely (i think)
     for stream in listener.incoming() {
@@ -39,69 +52,57 @@ fn listener_thread(listener: TcpListener, dest: String, receiver: mpsc::Receiver
         };
         //the receiver attempts to get a message from the server_switch thread
         //about the status of the server, then sets server_on to that status
-        //true if the server is on, false if it is off.
-        let server_on = match receiver.try_recv() {
-            Ok(r) => r,
-            Err(..) => panic!("Could not receive server_on signal!"),
-        };
+
         //attempts to pass packets between the two streams
-        match handle_tcp_connection(stream_source, stream_dest, server_on) {
+        match handle_tcp(stream_source, stream_dest, server_on.clone()) {
             Ok(..) => (),
             Err(..) => panic!("Could not handle tcp connection!"),
         }
         //handle_udp_connection (should this be its own thread?)
-        match sender.send(true) {
-            Ok(..) => (),
-            Err(..) => (),
-        } //sends an update to the server_switch indicating access
+        {
+            let mut update = server_update.lock().unwrap();
+            *update = true;
+        }
     }
 }
-//should never actually reach here so it should probably return error but :/
 
 //to be called by a server_switch thread. Continuously checks if the listener thread
 //has sent an update message. If 30 minutes have passed with no update message
 //an off signal should be sent to the destination machine.
-fn server_switch_thread(receiver: mpsc::Receiver<bool>, sender: mpsc::SyncSender<bool>) {
+fn server_switch_thread(server_on: Arc<Mutex<bool>>, server_update: Arc<Mutex<bool>>) {
     let mut time_last_handled = SystemTime::now();
-    let mut server_on = true;
     //server_switch loop
     loop {
-        //check if there's been an update message, if there has been set update = true
-        //if not, update = false
-        let update = match receiver.try_recv() {
-            Ok(..) => true,
-            _ => false,
-        };
         //if there's been an update, make time_last_handled the current time
         //since the listener thread is currently handling connections
-        if update {
-            time_last_handled = SystemTime::now();
+        {
+            let mut update = server_update.lock().unwrap();
+            if *update{
+                time_last_handled = SystemTime::now();
+            }
+            *update = false;
         }
         // if it has been more than 30 minutes since the last update, send an off signal
         //to the server machine.
         if time_last_handled.elapsed().unwrap() > Duration::from_secs(1800) {
             //send off signal
-            server_on = false;
-        }
-        //try to send a message indicating to the listener whether or not the server is on
-        //if the listener is not looking for this message, the thread is not blocked
-        match sender.try_send(server_on) {
-            _ => (),
+            let mut server_signal = server_on.lock().unwrap();
+            *server_signal = false;
         }
     }
 }
 
-//wake the server machine if its off and pass the connection :)
-fn handle_tcp_connection(mut stream_source: TcpStream, mut stream_dest: TcpStream, server_on: bool)
+//wake the server machine if its off and pass the packet :)
+fn handle_tcp(mut stream_source: TcpStream, mut stream_dest: TcpStream, server_on: Arc<Mutex<bool>>)
                          -> std::io::Result<()> {
-    if !server_on {
-        //send on signal
+    {
+        let server_status = server_on.lock().unwrap();
+        if !*server_status {
+            //send on signal
+        }
     }
     let mut buffer_source: Vec<u8> = vec![0; 4096];
-    let mut buffer_dest: Vec<u8> = vec![0; 4097];
     stream_source.read_to_end(&mut buffer_source)?;
-    stream_dest.read_to_end(&mut buffer_dest)?;
     stream_dest.write(&mut buffer_source)?;
-    stream_source.write(&mut buffer_dest)?;
     Ok(())
 }
